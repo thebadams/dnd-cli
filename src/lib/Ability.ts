6f3f64a7c9e46a2d2bae2b642053fe14f7caaa17
TheBadams <bpeteradams@gmail.com>
2022-01-04 14:23:36 -0500

    Ability(Config): Adjusted constructor of Ability and Config method to accomodate a name property on instances of the class; added abilities type, and Abilities enum; set static property ABILITIES to be Abilities enum

--- src/lib/Ability.ts
+++ src/lib/Ability.ts
@@ -1,8 +1,25 @@
+enum Abilities {
+	STR = 'Strength',
+	DEX = 'Dexterity',
+	CON = 'Constitution',
+	INT = 'Intelligence',
+	WIS = 'Wisdom',
+	CHA = 'Charisma'
+};
+
+type abilities = 'Strength' | 'Dexterity' | 'Constitution' | 'Intelligence' | 'Wisdom' | 'Charisma';
+
+interface IAbilityConfig {
+	name: abilities
+}
+
 export default class Ability {
-	constructor() {
-		
+	name: abilities;
+	constructor(name: abilities) {
+		this.name = name
 	}
-	public static Config() {
-		return new Ability()
+	private static ABILITIES = Abilities
+	public static Config(config: IAbilityConfig) {
+		return new Ability(config.name)
 	}
 }
