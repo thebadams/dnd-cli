80269eb27098da1b0ee3bef6e00d1087c43c8519
TheBadams <bpeteradams@gmail.com>
2022-01-04 15:04:16 -0500

    Test(Config): Wrote test to confirm that Config method returns an instance of Ability with proper name property

--- src/lib/Ability.ts
+++ src/lib/Ability.ts
@@ -1,4 +1,4 @@
-enum Abilities {
+export enum Abilities {
 	STR = 'Strength',
 	DEX = 'Dexterity',
 	CON = 'Constitution',
@@ -7,19 +7,39 @@ enum Abilities {
 	CHA = 'Charisma'
 };
 
-type abilities = 'Strength' | 'Dexterity' | 'Constitution' | 'Intelligence' | 'Wisdom' | 'Charisma';
+export type abilities = 'Strength' | 'Dexterity' | 'Constitution' | 'Intelligence' | 'Wisdom' | 'Charisma';
 
-interface IAbilityConfig {
-	name: abilities
+export interface IAbilityConfig {
+	name: abilities;
+	score: number;
 }
 
 export default class Ability {
 	name: abilities;
-	constructor(name: abilities) {
-		this.name = name
+	score: number;
+	constructor(name: abilities, score: number) {
+		this.name = name,
+		this.score = score
 	}
 	private static ABILITIES = Abilities
+	private static checkAbilityScore(score: number) {
+		if(score > 0) {
+			return score
+		} else {
+			throw Error('Invalid Score, Must Be a Positive Number')
+		}
+	}
 	public static Config(config: IAbilityConfig) {
-		return new Ability(config.name)
+		try {
+			const score = this.checkAbilityScore(config.score)
+			return new Ability(config.name, score);
+		} catch (error) {
+			if(error instanceof Error){
+				console.log(error)
+				return
+			}
+			
+		}
+		
 	}
 }
--- test/lib/Ability.test.ts
+++ test/lib/Ability.test.ts
@@ -1,13 +1,19 @@
-import Ability from '../../src/lib/Ability';
+import Ability, { Abilities, IAbilityConfig } from '../../src/lib/Ability';
 
 describe('Ability Class', () => {
 	test('Ability Should Exist', () => {
 		expect(Ability).toBeDefined();
 	})
 	describe('Static Config Method', () => {
-		const newAbility : Ability = Ability.Config()
+		const abilityConfig : IAbilityConfig = {
+			name: Abilities.STR
+		}
+		const newAbility : Ability = Ability.Config(abilityConfig);
 		test('Ability Should Have A Static Method, Config, That Returns A New Instance Of the Ability Class', () =>{
 			expect(newAbility).toBeInstanceOf(Ability);
-		})
-	})
-})
+		});
+		test('newAbility Should Have A Property, name, Equal to Abilities.STR', () => {
+			expect(newAbility).toHaveProperty('name', Abilities.STR);
+		});
+	});
+});
