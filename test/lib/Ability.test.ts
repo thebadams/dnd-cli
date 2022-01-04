37a5d52a5f54b801d0fb5c1124ac0a7bcc5b292b
TheBadams <bpeteradams@gmail.com>
2022-01-04 14:11:26 -0500

    Test(Ability.Config): Wrote test to confirm that Ability.Config returns an instance of the Ability class

--- test/lib/Ability.test.ts
+++ test/lib/Ability.test.ts
@@ -4,4 +4,10 @@ describe('Ability Class', () => {
 	test('Ability Should Exist', () => {
 		expect(Ability).toBeDefined();
 	})
+	describe('Static Config Method', () => {
+		const newAbility : Ability = Ability.Config()
+		test('Ability Should Have A Static Method, Config, That Returns A New Instance Of the Ability Class', () =>{
+			expect(newAbility).toBeInstanceOf(Ability);
+		})
+	})
 })
